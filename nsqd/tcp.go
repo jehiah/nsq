e9ba0b486844e426918065e897b8a36cca5c6aaf
Jehiah Czebotar <jehiah@gmail.com>
2026-08-04 23:37:20 -0400

    Fix TCP shutdown hang: track pending (pre-handshake) connections

--- nsqd/tcp.go
+++ nsqd/tcp.go
@@ -19,12 +19,21 @@ type Client interface {
 }
 
 type tcpServer struct {
-	nsqd  *NSQD
+	nsqd *NSQD
+	// conns holds fully handshaked clients, keyed by remote addr
 	conns sync.Map
+	// pendingConns holds raw connections that have been accepted but have
+	// not yet completed the protocol handshake (read their 4-byte magic),
+	// keyed by remote addr. Close() must be able to reach these too, since
+	// a connection can sit here indefinitely if the client never sends
+	// data.
+	pendingConns sync.Map
 }
 
 func (p *tcpServer) Handle(conn net.Conn) {
 	p.nsqd.logf(LOG_INFO, "TCP: new client(%s)", conn.RemoteAddr())
+
+	p.pendingConns.Store(conn.RemoteAddr(), conn)
 
 	// The client should initialize itself by sending a 4 byte sequence indicating
 	// the version of the protocol that it intends to communicate, this will allow us
@@ -32,6 +41,7 @@ func (p *tcpServer) Handle(conn net.Conn) {
 	buf := make([]byte, 4)
 	_, err := io.ReadFull(conn, buf)
 	if err != nil {
+		p.pendingConns.Delete(conn.RemoteAddr())
 		p.nsqd.logf(LOG_ERROR, "failed to read protocol version - %s", err)
 		_ = conn.Close()
 		return
@@ -46,6 +56,7 @@ func (p *tcpServer) Handle(conn net.Conn) {
 	case "  V2":
 		prot = &protocolV2{nsqd: p.nsqd}
 	default:
+		p.pendingConns.Delete(conn.RemoteAddr())
 		_, _ = protocol.SendFramedResponse(conn, frameTypeError, []byte("E_BAD_PROTOCOL"))
 		_ = conn.Close()
 		p.nsqd.logf(LOG_ERROR, "client(%s) bad protocol magic '%s'",
@@ -55,6 +66,7 @@ func (p *tcpServer) Handle(conn net.Conn) {
 
 	client := prot.NewClient(conn)
 	p.conns.Store(conn.RemoteAddr(), client)
+	p.pendingConns.Delete(conn.RemoteAddr())
 
 	err = prot.IOLoop(client)
 	if err != nil {
@@ -68,6 +80,10 @@ func (p *tcpServer) Handle(conn net.Conn) {
 func (p *tcpServer) Close() {
 	p.conns.Range(func(k, v interface{}) bool {
 		_ = v.(protocol.Client).Close()
+		return true
+	})
+	p.pendingConns.Range(func(k, v interface{}) bool {
+		_ = v.(net.Conn).Close()
 		return true
 	})
 }
